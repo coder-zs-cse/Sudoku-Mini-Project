5e3bb58c0e797468cabbf12c195a18df9bfabc4f
Zubin Shah <www.zubinshah1886@gmail.com>
2022-02-24 19:20:31 +0530

    Added Difficulty modes

--- Game/sudoku_game.c
+++ Game/sudoku_game.c
@@ -1,6 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+
+typedef struct {
+    int puzzle[9][9];
+    int solution[9][9];
+}sudoku;
+
+void init(sudoku *puzzle){
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            puzzle->puzzle[i][j]=0;
+        }
+    }
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            puzzle->solution[i][j]=0;
+        }
+    }
+}
+void copy_box1_to_box2(int box1[9][9],int box2[9][9]){
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            box2[i][j]=box1[i][j];
+        }
+    }
+}
+
+
 #define take(n) int n;scanf("%d",&n)
 void printbox(int box[9][9]){
     for(int i=0;i<3;i++)printf(" ");
@@ -111,7 +138,7 @@ int clash(int box[9][9],int i,int j,int num){
     if(boxclash(box,i,j,num)) return 1;
     return 0;
 }
-int duplicate_grid(int box1[9][9],int box2[9][9]){
+int is_box1_equal_to_box2(int box1[9][9],int box2[9][9]){
     for(int i=0;i<9;i++){
         for(int j=0;j<9;j++){
             if(box1[i][j]!=box2[i][j]) return 0;
@@ -147,12 +174,12 @@ int select_random_number(int box[9][9],int numvisited[10],int no_of_num_left){
     numvisited[num]=0;
     return num;
 }
-int select_random_grid(int box[9][9],int grid[82],int no_of_grids_left){
+int select_random_grid(int visited[82],int no_of_grids_left){
     int n=no_of_grids_left;
     int number = (rand() % n) + 1;
     int count1=0,index;
     for(int it=1;it<=81;it++){
-        if(grid[it]==1){
+        if(visited[it]==1){
             count1++;
         }
         if(count1==number){
@@ -160,61 +187,57 @@ int select_random_grid(int box[9][9],int grid[82],int no_of_grids_left){
             break;
         }
     }
-    grid[index] = 0;
+    visited[index] = 0;
     return index;
 }
-int solve_sudoku(int box[9][9],int i,int j,int S,int E,int X){
+int solve_sudoku(sudoku *matrix,int i,int j,int S,int E,int X){
     if(i==8 && j==9)  {
         return 1;
     }
-    if(j==9)  return solve_sudoku(box,i+1,0,S,E,X);
-    if(box[i][j]!=0) return solve_sudoku(box,i,j+1,S,E,X);
+    if(j==9)  return solve_sudoku(matrix,i+1,0,S,E,X);
+    if(matrix->solution[i][j]!=0) return solve_sudoku(matrix,i,j+1,S,E,X);
     
     for(int num = S;num != E + X;num = num + X){
-        if(!clash(box,i,j,num)){
-            box[i][j]=num;
-            if(solve_sudoku(box,i,j+1,S,E,X)){
+        if(!clash(matrix->solution,i,j,num)){
+            matrix->solution[i][j]=num;
+            if(solve_sudoku(matrix,i,j+1,S,E,X)){
                 return 1;
             }
-            box[i][j]=0;
+            matrix->solution[i][j]=0;
         }
     }
     return 0;
 }
 int unique_solution(int box[9][9]){
-    int tempbox1[9][9];
-    int tempbox2[9][9];
-    for(int i=0;i<9;i++){
-        for(int j=0;j<9;j++){
-            tempbox1[i][j]=box[i][j];
-            tempbox2[i][j]=box[i][j];
-        }
-    }
-    solve_sudoku(tempbox1,0,0,1,9,1);
-    solve_sudoku(tempbox2,0,0,9,1,-1);
-    if(duplicate_grid(tempbox1,tempbox2)) return 1; //if both solutions are equal, that means unique solution
+    sudoku tempbox1;
+    sudoku tempbox2;
+    copy_box1_to_box2(box,tempbox1.solution);
+    copy_box1_to_box2(box,tempbox2.solution);
+    solve_sudoku(&tempbox1,0,0,1,9,1);
+    solve_sudoku(&tempbox2,0,0,9,1,-1);
+    if(is_box1_equal_to_box2(tempbox1.solution,tempbox2.solution)) return 1; //if both solutions are equal, that means unique solution
     return 0;
 }
-void puzzle_generator(int box[9][9],int grid[82],int steps){
+void puzzle_generator(sudoku *matrix,int visited[82],int steps){
     if(steps==0) return ;
-    int index = select_random_grid(box,grid,steps);
+    int index = select_random_grid(visited,steps);
     int x = (index-1)/9;
     int y = (index-1)%9;
-    int store = box[x][y];
-    box[x][y] = 0;
-    if(!unique_solution(box)){
-        box[x][y]=store;    //restore 
+    int store = matrix->puzzle[x][y];
+    matrix->puzzle[x][y] = 0;
+    if(!unique_solution(matrix->puzzle)){
+        matrix->puzzle[x][y]=store;    //restore 
     }
-    puzzle_generator(box,grid,steps-1);
+    puzzle_generator(matrix,visited,steps-1);
 }
-void generate_puzzle(int box[9][9]){
-    int check[82];
+void generate_puzzle(sudoku *matrix){
+    int visited[82];
     for(int i=0;i<=81;i++){
-        check[i]=1;
+        visited[i]=1;
     }
-    puzzle_generator(box,check,81);
+    puzzle_generator(matrix,visited,81);
 }
-void diagonal_solve(int box[9][9]){
+void diagonal_solve(sudoku *puzzle){
     for(int diag =0 ;diag<=6;diag+=3){
         int numvisited[10];
         int cellvisited[10];
@@ -222,11 +245,11 @@ void diagonal_solve(int box[9][9]){
             numvisited[i] = cellvisited[i] = 1;
         }
         for(int emptyboxes=9;emptyboxes>=1;emptyboxes--){
-            int cell = select_random_number(box,cellvisited,emptyboxes);
-            int num = select_random_number(box,numvisited,emptyboxes);
+            int cell = select_random_number(puzzle->solution,cellvisited,emptyboxes);
+            int num = select_random_number(puzzle->solution,numvisited,emptyboxes);
             int x = (cell-1)/3;
             int y = (cell-1)%3;
-            box[diag+x][diag+y] = num;
+            puzzle->solution[diag+x][diag+y] = num;
         }
     }
 }
@@ -236,20 +259,20 @@ int validinput(int box[9][9],int i,int j,int num){
     if(clash(box,i,j,num)) return 0;
     return 1;
 }
-void get_a_hint(int box[9][9],int solution[9][9]){
+void get_a_hint(sudoku *current,sudoku *matrix){
     int count=0;
     for(int i=0;i<9;i++){
         for(int j=0;j<9;j++){
-            if(box[i][j]==0) count++;
+            if(current->puzzle[i][j]==0) count++;
         }
     }  
     int number = rand()%count +1;
     for(int i=0;i<9;i++){
         for(int j=0;j<9;j++){
-            if(box[i][j]==0) number--;
+            if(current->puzzle[i][j]==0) number--;
             if(number==0){
-                printf("At (%d,%d) the number is %d\n",i,j,solution[i][j]);
-                box[i][j] = solution[i][j];
+                printf("At (%d,%d) the number is %d\n",i,j,matrix->solution[i][j]);
+                current->puzzle[i][j] = matrix->solution[i][j];
                 return;
             }
         }
@@ -283,16 +306,57 @@ void takeinput(int box[9][9]){
         printf("Invalid entry! Try again\n");
     }
 }
-void play(int puzzle[9][9]){
-    int box[9][9];
-    int solution[9][9];
+void fill_k_empty_boxes(sudoku *matrix,int k){
+    int total_empty_boxes=0;
     for(int i=0;i<9;i++){
         for(int j=0;j<9;j++){
-            solution[i][j] = box[i][j] = puzzle[i][j];
+            if(matrix->puzzle[i][j]==0) total_empty_boxes++;
+        }
+    }  
+    while(k--){
+        int number = rand()%total_empty_boxes +1;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(matrix->puzzle[i][j]==0) number--;
+                if(number==0){
+                    matrix->puzzle[i][j] = matrix->solution[i][j];
+                    goto out;
+                }
+            }
+        }
+        out:
+        total_empty_boxes--;
+    }
+}
+void difficulty_mode(sudoku *matrix){
+    int k;
+    while(1){
+        int choice;
+        printf("Press 1 to play a easy mode sudoku game\n");
+        printf("Press 2 to play a medium mode sudoku game\n");
+        printf("Press 3 to play a hard mode sudoku game\n");
+        scanf("%d",&choice);
+        switch (choice)
+        {
+        case 1: k = rand() % 10 + 20;
+            goto out;
+        case 2: k = rand() % 10 + 10;
+            goto out;
+        case 3: 
+            return;
+        default:
+        printf("Invalid entry!, please check the menu and try again\n");
         }
     }
-    solve_sudoku(solution,0,0,1,9,1);
-    while(!duplicate_grid(box,solution)){
+    out:
+    fill_k_empty_boxes(matrix,k);
+}
+void play_sudoku(sudoku *matrix){
+    difficulty_mode(matrix);
+    printbox(matrix->puzzle);
+    sudoku current;
+    copy_box1_to_box2(matrix->puzzle,current.puzzle);
+    while(!is_box1_equal_to_box2(current.puzzle,matrix->solution)){
         int choice;
         printf("Press 1 to enter your next input\n");
         printf("Press 2 to get a hint\n");
@@ -302,42 +366,40 @@ void play(int puzzle[9][9]){
         printf("\n");
         scanf("%d",&choice);
         switch(choice){
-            case 1: takeinput(box);
+            case 1: takeinput(current.puzzle);
             break;
-            case 2: get_a_hint(box,solution);
+            case 2: get_a_hint(&current,matrix);
             break;
-            case 3: undo_move(box,puzzle);
+            case 3: undo_move(current.puzzle,matrix->puzzle);
+            break;
+            case 4: printparallel(current.puzzle,matrix->solution);
+                    return;
             break;
             case -1: return;
             break;
-            case 4: printparallel(box,solution);
-            break;
             defult: printf("Invalid entry, Please check menu\n");
         }
-        printbox(box);
+        printbox(current.puzzle);
     }
     printf("Congratulations!\n You have solved the puzzle.\n");
 }
-void solve(){
-    int box[9][9];
-    for(int i=0;i<9;i++){
-        for(int j=0;j<9;j++){
-            box[i][j]=0;
-        }
-    }
-    diagonal_solve(box);
-    solve_sudoku(box,0,0,1,9,1);
-    generate_puzzle(box);
-    printbox(box);
-    play(box);
+void play(){
+    sudoku matrix;
+    init(&matrix);
+    diagonal_solve(&matrix);
+    solve_sudoku(&matrix,0,0,1,9,1);
+    copy_box1_to_box2(matrix.solution,matrix.puzzle);
+    generate_puzzle(&matrix);
+    play_sudoku(&matrix);
 }
 void input(){
-    int puzzle[9][9];
+    sudoku matrix;
     for(int i=0;i<9;i++){
         for(int j=0;j<9;j++){
-            scanf("%d",&puzzle[i][j]);
+            scanf("%d",&matrix.puzzle[i][j]);
         }
     }
+
     while(1){
         int choice;
         printf("Press 1 if you want to solve the inputted puzzle yourself\n");
@@ -345,10 +407,10 @@ void input(){
         printf("Press -1 to exit\n");
         scanf("%d",&choice);
         switch(choice){
-            case 1: play(puzzle);
+            case 1: play_sudoku(&matrix);
             break; 
-            case 2: solve_sudoku(puzzle,0,0,1,9,1);
-                    printbox(puzzle);
+            case 2: solve_sudoku(&matrix,0,0,1,9,1);
+                    printbox(matrix.solution);
             break;
             case -1: return;
             default : 
@@ -365,7 +427,7 @@ int main(){
         printf("Press -1 to exit\n");
         scanf("%d",&choice);
         switch(choice){
-            case 1: solve();
+            case 1: play();
             break;
             case 2: input();
             break;
